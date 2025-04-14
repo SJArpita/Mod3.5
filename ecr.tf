43a02ef119297716eb394c1f77e8e73db4cd52d5
SJArpita <rsheea12@gmail.com>
2025-04-14 20:30:08 +0800

    initial commit

---
+++ ecr.tf
@@ -0,0 +1,30 @@
+# ---ECR created manually, therefore tf create is not needed
+# ----------------------------------------------------------
+/* 
+aws ecr create-repository \
+  --repository-name flask-ecr-demo \
+  --image-scanning-configuration scanOnPush=true \
+  --region us-east-1
+ */
+
+
+# ---Create the ECR Repository with image scanning enabled
+# --------------------------------------------------------
+/* 
+resource "aws_ecr_repository" "flask_ecr_demo" {
+  name = "flask-ecr-demo"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  # Optionally, you can set encryption and lifecycle policies if needed.
+  encryption_configuration {
+    encryption_type = "AES256" # Default is AES256 encryption, you can also use KMS.
+  }
+
+  lifecycle {
+    prevent_destroy = false # Allows the resource to be destroyed
+  }
+}
+*/
